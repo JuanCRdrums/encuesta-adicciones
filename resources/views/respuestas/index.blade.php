7997f0892f583d5358d40c4db3a46cc7284cb698
JuanCRdrums <juankdrums14@gmail.com>
2021-05-31 21:00:40 -0500

    Se agrega Datatables de jQuery para que se puedan exportar las tablas a Excel

--- resources/views/respuestas/index.blade.php
+++ resources/views/respuestas/index.blade.php
@@ -30,7 +30,7 @@
         <div class="row">
             <div class="col-md-auto">
                 <div class="table-responsive">
-                    <table class="table table-striped">
+                    <table data-sortcols='{"0":"asc","1":"asc"}' class="table datatables_tools table-striped">
                         <thead>
                             <th>Nombre</th>
                             <th>Teléfono</th>
@@ -77,6 +77,15 @@
 {!! Html::script('plugins/datatables/Buttons/js/dataTables.buttons.min.js') !!} 
 {!! Html::script('plugins/datatables/Buttons/js/buttons.print.min.js') !!}
 {!! Html::script('plugins/datatables/Buttons/js/buttons.flash.min.js') !!}
+{!! Html::script('js/jquery.validate.min.js') !!}
+{!! Html::script('js/jquery.dataTables.min.js') !!}
+{!! Html::script('js/dataTables.bootstrap.min.js') !!} 
+{!! Html::script('js/dataTables.buttons.min.js') !!} 
+{!! Html::script('js/jszip.min.js') !!}
+{!! Html::script('js/pdfmake.min.js') !!}
+{!! Html::script('js/vfs_fonts.js') !!}
+{!! Html::script('js/buttons.html5.min.js') !!} 
+{!! Html::script('js/buttons.colVis.min.js') !!}
 @stop
 
 <style>
